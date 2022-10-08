b24498c5db3765406bb5573b8916e97b6f2258a9
Büşra Talih <busra.talihhh@gmail.com>
2022-10-08 15:57:51 +0300

    first hello

---
+++ src/javaDers18/Loops_DoWhile.java
@@ -0,0 +1,23 @@
+package javaDers18;
+
+public class Loops_DoWhile {
+    public static void main(String[] args) {
+
+        int sayi=1;
+
+       do {
+           System.out.println("Merhaba");
+           sayi++;
+       }while (sayi<5); //false
+
+        System.out.println("alt satir");
+
+
+
+
+
+
+
+
+    }
+}
